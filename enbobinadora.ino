e7a56dbe6f3ecb242a7d287cd289a4f81145a218
David <sotodelanoche@gmail.com>
2015-02-03 13:08:21 +0100

    Update enbobinadora.ino

--- enbobinadora.ino
+++ enbobinadora.ino
@@ -1,30 +1,30 @@
 #include <LiquidCrystal.h>
-
+//pin
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int motor=9;
 int boton=7;
 int botonval=0;
 int pad=A5;
-int padval=0;
-int contador;
-int menu=0;
-int espiras=0;
-int diametro1=0;
-int diametro=0;
-int largo1=0;
-int largo=0;
-float uH=0;
-int ok=0;
-int val=0;
-int val1=0;
-char D[3];
+//variables
+int padval=0; //variable valor boton 
+int contador; //contador de vueltas
+int menu=0;// variable menu
+int espiras=0; // numero de vueltas 
+int diametro1=0;// decenas de diametro  unidades,decena representacion en lcd
+int diametro=0;// unidades de diametro
+int largo1=0; // decenas de largo bobina
+int largo=0; // unidades de largo bobina 
+float uH=0;// resultado de la capacidad de la bobina
+int ok=0;// //variable simple 1 o 0 
+int val=0; // variable de calculos
+int val1=0;//variable de calculos
+char D[3];//variables de muestra en pantalla sprintf
 char D1[3];
 char L[3];
 char L1[3];
 
-void setup() {
-
-  Serial.begin(9600);
+void setup() {// configuraciion de la placa 
+  
   lcd.begin(16, 2);
   pinMode(pad,INPUT);
   pinMode(boton,INPUT); 
@@ -37,9 +37,9 @@ void setup() {
 
 void loop() {
 
-  switch (menu) {
+  switch (menu) {// menu de pantalla
   case 1:
-    padN();
+    padN();// funcion pad para cambiar el valor de espiras
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("N=");
@@ -47,7 +47,7 @@ void loop() {
     lcd.print(espiras);
     break;
   case 2:
-    padD();
+    padD();// funcion pad para cambiar el valor de diametro unidades
     lcd.home();
     lcd.setCursor(0,0);
     lcd.print("D=");
@@ -63,7 +63,7 @@ void loop() {
     lcd.print("CM");
     break;
   case 3:
-    padD1();
+    padD1(); // funcion pad para cambiar el valor de diametro decenas
     lcd.home();
     lcd.setCursor(0,0);
     lcd.print("D=");
@@ -80,7 +80,7 @@ void loop() {
 
     break;
   case 4:
-    padL();
+    padL();// funcion pad para cambiar el valor de largo unidades
     lcd.home();
     lcd.setCursor(0,0);
     lcd.print("L=");
@@ -97,7 +97,7 @@ void loop() {
     lcd.print("CM");
     break;
   case 5:
-    padL1();
+    padL1();// funcion pad para cambiar el valor de decenas
     lcd.home();
     lcd.setCursor(0,0);
     lcd.print("L=");
@@ -115,7 +115,7 @@ void loop() {
     break;
 
   default: 
-    padM();
+    padM();// menu principal 
     lcd.setCursor(0, 0);
     lcd.print("MENU N D L"); 
   }
